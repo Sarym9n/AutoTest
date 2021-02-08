b75d004bf98d4611c639baf62813fba6a7861be0
Sarym9n <63037205+Sarym9n@users.noreply.github.com>
2021-02-08 23:04:07 +0300

    добавлены методы для объектов

--- San/src/main/java/ru/stqa/AutoTest/San/MyFirstProgram.java
+++ San/src/main/java/ru/stqa/AutoTest/San/MyFirstProgram.java
@@ -4,27 +4,16 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     hello("Воин");
-    hello("Дмитрий");
-    hello("Василий");
 
-    double l = 5;
-    System.out.println("Площадь " + l + " = " + area(l));
+    Squere s = new Squere(5);
+    System.out.println("Площадь " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника " + a + " и " + b + " = " + area(a,b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника " + r.a + " и " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Приветствую " + somebody);
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
   }
 
 }
---
+++ San/src/main/java/ru/stqa/AutoTest/San/Squere.java
@@ -0,0 +1,14 @@
+package ru.stqa.AutoTest.San;
+
+public class Squere {
+
+  public double l;
+
+  public Squere(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}

7fe21df812c8ddb22a661a46dd4c1913212c6bb0
Sarym9n <63037205+Sarym9n@users.noreply.github.com>
2021-02-08 22:21:36 +0300

    добавлено несколько фун-ий

--- San/src/main/java/ru/stqa/AutoTest/San/MyFirstProgram.java
+++ San/src/main/java/ru/stqa/AutoTest/San/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.AutoTest.San;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Приветствую");
+    hello("Воин");
+    hello("Дмитрий");
+    hello("Василий");
+
+    double l = 5;
+    System.out.println("Площадь " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника " + a + " и " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Приветствую " + somebody);
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
